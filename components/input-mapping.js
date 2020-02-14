b7f428e7634a00368aee632da1b4e27dc1b82215
Thomas Pingel <thomas.pingel@gmail.com>
2020-02-13 23:23:07 -0500

    Better teleport

--- components/input-mapping.js
+++ components/input-mapping.js
@@ -1,22 +1,20 @@
-    AFRAME.registerComponent('input-listen', {
-        init:
-            function () {
+AFRAME.registerComponent('input-listen', {
+	init:
+		function () {
 
 
-                //X-buttorn Pressed 
-                this.el.addEventListener('bbuttondown', function (e) {
-                    //Start pointing position to teleport  
-                    this.emit('teleportstart');
-                });
+			//X-buttorn Pressed 
+			this.el.addEventListener('bbuttondown', function (e) {
+				//Start pointing position to teleport  
+				this.emit('teleportstart');
+			});
 
-                //X-buttorn Released 
-                this.el.addEventListener('bbuttonup', function (e) {
-                    //Jump to pointed position
-                    this.emit('teleportend');
-                });
+			//X-buttorn Released 
+			this.el.addEventListener('bbuttonup', function (e) {
+				//Jump to pointed position
+				this.emit('teleportend');
+			});
 
-                //console.log(this);
-            },
-
-
-    });
+			//console.log(this);
+		}
+});
